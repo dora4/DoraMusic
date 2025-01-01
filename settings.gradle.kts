f754f6d797d1cb4ad4f65d9c6f32d6877b0d7beb
dora <dora924666990@gmail.com>
2025-01-02 02:17:07 +0800

    CI构建允许不安全的协议

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android.tools.build") {
-                useModule("com.android.tools.build:gradle:7.1.2")
+                useModule("com.android.tools.build:gradle:8.1.0")
             }
             if (requested.id.namespace == "com.google.firebase") {
                 useModule("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
@@ -25,9 +25,13 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven {  setUrl("https://jitpack.io") }
-
         // 蒲公英的仓库
-        maven { setUrl("https://frontjs-static.pgyer.com/dist/sdk/pgyersdk") }
+        maven {
+            // GitHub CI/CD 环境对 Cookie 的管理可能会更加严格，当域名不合法时会直接拒绝使用。该仓库没有遵循
+            // RFC 6265 标准，所以要允许不安全的协议
+            isAllowInsecureProtocol = true
+            setUrl("https://frontjs-static.pgyer.com/dist/sdk/pgyersdk")
+        }
     }
 }
 rootProject.name = "DoraMusic"
