47fc424ca3fb1932ed7f71fac56158ca3212a24c
dora <dora924666990@gmail.com>
2025-06-16 03:56:31 +0800

    优化文字显示

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 34
 
-    val code = 28
-    val version = "1.3.14"
+    val code = 29
+    val version = "1.3.15"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 23
