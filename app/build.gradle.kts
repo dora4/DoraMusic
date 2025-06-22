40dfd347f9b82130ddddd1a6705ca9b8b30b248a
dora <dora924666990@gmail.com>
2025-06-23 01:28:38 +0800

    修复自动VPN连接开关无效。(https://github.com/dora4/DoraMusic/issues/18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 34
 
-    val code = 36
-    val version = "1.4.0"
+    val code = 37
+    val version = "1.4.1"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 23
