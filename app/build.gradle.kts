81cca42d17661f841f496aa6d9db7fa48519d6c8
dora <dora924666990@gmail.com>
2025-07-21 20:23:02 +0800

    修改用户条款。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 34
 
-    val code = 47
-    val version = "1.5.2"
+    val code = 48
+    val version = "1.5.3"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 23
