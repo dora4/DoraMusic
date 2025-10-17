8ee4dd047f66907a0a1a68c6deb287956a18587f
dora <dora924666990@gmail.com>
2025-10-17 21:39:03 +0800

    大幅优化UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 34
 
-    val code = 69
-    val version = "1.6.4"
+    val code = 70
+    val version = "1.6.5"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 23
