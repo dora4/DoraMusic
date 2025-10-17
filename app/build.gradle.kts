bdf3e781a84ee5ff512adf412c358ff30abf66e7
dora <dora924666990@gmail.com>
2025-10-17 17:30:32 +0800

    升级支付sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 34
 
-    val code = 68
-    val version = "1.6.3"
+    val code = 69
+    val version = "1.6.4"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 23
