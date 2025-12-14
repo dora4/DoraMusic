00ff4533b598df4c38e4f04af2252700ef732c09
Dora Chat <dora924666990@gmail.com>
2025-12-15 03:23:16 +0700

    增加注释

--- app/src/main/java/site/doramusic/app/sort/Sort.java
+++ app/src/main/java/site/doramusic/app/sort/Sort.java
@@ -1,5 +1,8 @@
 package site.doramusic.app.sort;
 
+/**
+ * 按字母排序的接口，用于排序。
+ */
 public interface Sort extends Comparable<Sort> {
 
     /**
@@ -9,5 +12,10 @@ public interface Sort extends Comparable<Sort> {
      */
     void setSortLetter(String sortLetter);
 
+    /**
+     * 获取用于排序的属性的字母。
+     *
+     * @return 用于排序的属性的字母
+     */
     String getSortLetter();
 }
