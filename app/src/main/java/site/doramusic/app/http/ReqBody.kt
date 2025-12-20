fe1543854677b8d3588b06d6da0d9b93c96104e2
Dora Chat <dora924666990@gmail.com>
2025-12-20 13:48:14 +0700

    完善ReqBody

--- app/src/main/java/site/doramusic/app/http/ReqBody.kt
+++ app/src/main/java/site/doramusic/app/http/ReqBody.kt
@@ -1,8 +1,18 @@
 package site.doramusic.app.http
 
+import com.google.gson.Gson
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
 data class ReqBody(
     val mode: String = "",
     val key: String = "",
     val data: String = "",
     val sign: String = "",
-)
+) {
+
+    fun toRequestBody() : RequestBody {
+        return Gson().toJson(this).toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+    }
+}
