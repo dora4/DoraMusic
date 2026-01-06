6e039f27a2279304b8fa1523fb8bcf5908b42232
Dora Chat <dora924666990@gmail.com>
2026-01-06 14:42:18 +0700

    稳定性提升

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 36
 
-    val code = 108
-    val version = "1.9.6"
+    val code = 109
+    val version = "1.9.7"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 24
