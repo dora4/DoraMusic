b4189b63ee70e10c637a3494da4eeb099a6d822c
Dora Chat <dora924666990@gmail.com>
2026-01-09 04:29:06 +0700

    提升版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 36
 
-    val code = 111
-    val version = "2.0.0"
+    val code = 112
+    val version = "2.0.1"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 24
