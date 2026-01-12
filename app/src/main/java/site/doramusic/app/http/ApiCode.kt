9f6138677c23cff9d4704ff40d9b0800f2de643a
Dora Chat <dora924666990@gmail.com>
2026-01-13 03:02:58 +0700

    登录接口更改为Flow请求

--- app/src/main/java/site/doramusic/app/http/ApiCode.kt
+++ app/src/main/java/site/doramusic/app/http/ApiCode.kt
@@ -1,5 +1,9 @@
 package site.doramusic.app.http
 
+/**
+ * API响应吗。
+ * https://github.com/dora4/DoraMusic/blob/main/ERROR_CODE.md
+ */
 interface ApiCode {
     companion object {
         const val SUCCESS = "000000"
