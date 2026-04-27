e5d3883bd369092e7eeae2c90d1d0f3a2d94e044
Dora Chat <dora924666990@gmail.com>
2026-04-28 06:09:54 +0700

    颜色选择界面显示颜色名称（https://github.com/dora4/DoraMusic/issues/45）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 36
 
-    val code = 137
-    val version = "2.1.8"
+    val code = 138
+    val version = "2.1.9"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 24
