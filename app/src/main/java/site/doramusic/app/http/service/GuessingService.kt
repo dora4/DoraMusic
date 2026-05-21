10250800fe9a38da47e768bad397ab835e44d6fe
跟着哆啦学安卓 <71242257+dora4@users.noreply.github.com>
2026-05-22 01:03:07 +0700

    竞猜接口列表。（https://github.com/dora4/DoraMusic/issues/53）

--- app/src/main/java/site/doramusic/app/http/service/GuessingService.kt
+++ app/src/main/java/site/doramusic/app/http/service/GuessingService.kt
@@ -10,12 +10,30 @@ import retrofit2.http.Query
 import site.doramusic.app.http.ApiResult
 import site.doramusic.app.http.DoraGuessingInfoWithItems
 import site.doramusic.app.http.DoraGuessingRank
+import site.doramusic.app.http.DoraGuessingReward
+import site.doramusic.app.http.GuestSession
 
 interface GuessingService : ApiService {
 
     companion object {
+
+        /**
+         * 深情端口的朵拉音乐服务器。
+         */
         const val SERVER_URL = "http://dorachat.com:3000"
     }
+
+    /**
+     * 生成游客令牌。
+     */
+    @GET("guest/init")
+    fun initGuest(): Call<ApiResult<GuestSession>>
+
+    /**
+     * 检测游客令牌。
+     */
+    @GET("guest/checkToken")
+    fun checkGuestToken(@Query("token") token: String): Call<ApiResult<Boolean>>
 
     /**
      * 获取竞猜列表。
@@ -30,13 +48,19 @@ interface GuessingService : ApiService {
     fun bet(@Body body: RequestBody): Call<ApiResult<Boolean>>
 
     /**
+     * 我的竞猜，结算后的竞猜奖励结果列表。
+     */
+    @POST("guessing/reward")
+    fun getRewardList(@Body body: RequestBody): Call<ApiResult<MutableList<DoraGuessingReward>>>
+
+    /**
      * 领取积分。
      */
     @POST("guessing/claim")
     fun claim(@Body body: RequestBody): Call<ApiResult<Long>>
 
     /**
-     * 竞猜排行榜。0 - 胜率榜，1 - 盈利榜，2 - 投注榜。
+     * 竞猜排行榜。0 - 胜率榜，1 - 盈亏榜，2 - 投注榜。
      */
     @GET("guessing/rank")
     fun getRank(@Query("type") type: Int): Call<ApiResult<MutableList<DoraGuessingRank>>>
