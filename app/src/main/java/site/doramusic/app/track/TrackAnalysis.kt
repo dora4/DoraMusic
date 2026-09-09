f2348f45642382808c3d4aa0ab8d686c727fbec2
dora <dora924666990@gmail.com>
2026-09-09 18:57:04 +0700

    修复埋点统计报错

--- app/src/main/java/site/doramusic/app/track/TrackAnalysis.kt
+++ app/src/main/java/site/doramusic/app/track/TrackAnalysis.kt
@@ -1,19 +1,25 @@
 package site.doramusic.app.track
 
+import android.os.Build
 import dora.http.DoraHttp.rxResult
 import dora.http.retrofit.RetrofitManager
+import dora.util.ApkUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import site.doramusic.app.conf.AppConfig
-import site.doramusic.app.feedback.DeviceInfoProvider
 import site.doramusic.app.http.SecureRequestBuilder
 
 object TrackAnalysis {
 
     fun report(scope: CoroutineScope, eventName: String) {
+        val brand = Build.BRAND ?: ""
+        val model = Build.MODEL ?: ""
+        val os = "Android"
+        val osVersion = Build.VERSION.RELEASE ?: ""
+        val appVersion = ApkUtils.getVersionName()
         scope.launch {
             val req = ReqTrackEvent(
-                AppConfig.PRODUCT_NAME, DeviceInfoProvider.collect().toString(),
+                AppConfig.PRODUCT_NAME, "$brand-$model-$os-$osVersion-$appVersion",
                 arrayListOf(
                     TrackEventItem(eventName, System.currentTimeMillis(), null)
                 )
