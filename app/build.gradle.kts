efee4928258b97f14daefae86bc4a65488016acd
dora <dora924666990@gmail.com>
2026-09-10 17:58:57 +0700

    提升版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 36
 
-    val code = 171
-    val version = "2.3.12-cn"
+    val code = 172
+    val version = "2.4.0-cn"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 24
