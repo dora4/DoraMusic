faa955d874741fb31032ebef2c025e898ee02ea3
dora <dora924666990@gmail.com>
2026-09-14 23:00:42 +0700

    新增当前播放歌曲的动画
    (cherry picked from commit 51c79560b56b2224b66dafaab91e669f1261a801)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     namespace = "site.doramusic.app"
     compileSdk = 36
 
-    val code = 174
-    val version = "2.4.2-cn"
+    val code = 175
+    val version = "2.4.3-cn"
     defaultConfig {
         applicationId = "site.doramusic.app"
         minSdk = 24
--- app/src/main/java/site/doramusic/app/ui/adapter/MusicItemAdapter.kt
+++ app/src/main/java/site/doramusic/app/ui/adapter/MusicItemAdapter.kt
@@ -1,11 +1,17 @@
 package site.doramusic.app.ui.adapter
 
+import android.view.View
+import android.widget.ProgressBar
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import dora.widget.DoraAudioWaveProgressBar
 
 import site.doramusic.app.util.MusicUtils
 
 import site.doramusic.app.R
+import site.doramusic.app.conf.AppConfig
 import site.doramusic.app.db.Music
+import site.doramusic.app.media.MediaManager
+import site.doramusic.app.util.ThemeSelector
 
 class MusicItemAdapter : BaseSortItemAdapter<Music>(R.layout.item_music) {
 
@@ -17,5 +23,15 @@ class MusicItemAdapter : BaseSortItemAdapter<Music>(R.layout.item_music) {
         holder.setText(R.id.tv_music_name, item.musicName)
         holder.setText(R.id.tv_music_artist, item.artist)
         holder.setText(R.id.tv_music_duration, MusicUtils.formatTime(item.duration.toLong()))
+        val progressBar = holder.getView<DoraAudioWaveProgressBar>(R.id.progressBar)
+        val skinThemeColor = ThemeSelector.getThemeColor(context)
+        progressBar.setColor(skinThemeColor)
+        if (MediaManager.curMusicId == item.songId
+            && MediaManager.playState == AppConfig.MPS_PLAYING
+        ) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
     }
 }
