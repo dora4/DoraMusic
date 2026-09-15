b4528cdd4c8ecec489e3f701347004b3bcc4f45e
dora <dora924666990@gmail.com>
2026-09-15 19:39:53 +0700

    修复依赖引用问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -145,6 +145,7 @@ dependencies {
     implementation("com.github.dora4:dview-radar-view:1.0")
     implementation("com.github.dora4:dview-banner-view:1.12")
     implementation("com.github.dora4:dview-marquee-view:1.1")
+    implementation("com.github.dora4:dview-wave-view:1.1")
 
     implementation("com.github.dora4:dorachat-authsdk:1.1.4")
 
